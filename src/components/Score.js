0f4f16c9550c29f8f8c3bf6b1f8f2ee681c9221c
nivinabbas <nivin.abbas1@gmail.com>
2021-12-21 22:08:15 +0200

    create Score component

---
+++ src/components/Score.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Score extends Component{
+    render(){
+        let score=100;
+        return(<div>
+            {score}
+        </div>)
+    }
+
+}
+export default Score;
