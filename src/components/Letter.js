49de8bccd3a9a92204b398b129b2131a0a927fd0
nivinabbas <nivin.abbas1@gmail.com>
2021-12-21 22:07:39 +0200

    create letter component

---
+++ src/components/Letter.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Letter extends Component{
+    render(){
+        return(<div>
+            <span>a</span>
+        </div>)
+    }
+
+}
+export default Letter;
