834aa3debcab9aa73520f175aa111bbba203fa18
nivinabbas <nivin.abbas1@gmail.com>
2021-12-27 16:37:15 +0200

    modify letters component

--- src/components/Letters.js
+++ src/components/Letters.js
@@ -1,13 +1,26 @@
-import React, {Component} from 'react';
-import Letter from './Letter';
+import Letter from "./Letter";
+import React, { Component } from "react";
 
-class Letters extends Component{
-    render(){
-        return(<div>
-          <h1> Available letters</h1> 
-            <Letter/>
-        </div>)
-    }
 
+class Letters extends Component {
+
+  clickedLetter = (letter) => {
+
+    this.props.clickedLetter(letter);
+  };
+
+  render() {
+    return (
+      <div>
+        {Object.keys(this.props.letterStatus).map((l) => 
+      
+        <Letter letter={l} key={l} clickedLetter={this.clickedLetter} >{l}</Letter>
+  )}
+      </div>
+    );
+  }
 }
+
+
+
 export default Letters;
