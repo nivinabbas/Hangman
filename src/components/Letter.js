ee73c05f33488d08005252b2282badcb487928ac
nivinabbas <nivin.abbas1@gmail.com>
2021-12-27 16:37:10 +0200

    modify letter component

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -1,11 +1,28 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class Letter extends Component{
-    render(){
-        return(<div>
-            <span>a</span>
-        </div>)
-    }
-
+class Letter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isClick: false,
+    };
+  }
+  clickedLetter = () => {
+    this.props.clickedLetter(this.props.letter);
+    this.setState({ isClick: true });
+  };
+  render() {
+    return this.state.isClick ? (
+      <span
+        style={{ textDecoration: 'line-through' }}
+        onClick={this.clickedLetter}
+      >
+        {this.props.letter}
+      </span>
+    ) : (
+      <span onClick={this.clickedLetter}>{this.props.letter}</span>
+    );
+  }
 }
+
 export default Letter;
